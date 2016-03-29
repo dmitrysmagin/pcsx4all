2b09ea63608369347c6d086d34c47cf4c0ec196a
Dmitry Smagin <exmortis@yandex.ru>
2016-03-29 14:36:15 +0300

    TMP: Add debug msg on each psxCpu->Clear() call

--- src/psxdma.cpp
+++ src/psxdma.cpp
@@ -60,7 +60,7 @@ void psxDma4(u32 madr, u32 bcr, u32 chcr) { // SPU
 			size = (bcr >> 16) * (bcr & 0xffff) * 2;
     		SPU_readDMAMem(ptr, size);
 			#ifdef PSXREC
-			psxCpu->Clear(madr, size);
+			CATCH(); psxCpu->Clear(madr, size);
 			#endif
 			break;
 
@@ -131,7 +131,7 @@ void psxDma2(u32 madr, u32 bcr, u32 chcr) { // GPU
 			size = (bcr >> 16) * (bcr & 0xffff);
 			GPU_readDataMem(ptr, size);
 			#ifdef PSXREC
-			psxCpu->Clear(madr, size);
+			CATCH(); psxCpu->Clear(madr, size);
 			#endif
 			break;
 
