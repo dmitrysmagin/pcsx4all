084a2cbaf647b725d2d3268274d72fe916881708
Dmitry Smagin <exmortis@yandex.ru>
2016-04-27 12:00:56 +0300

    mipsrec: Rearrange #ifdefs to reduce code

--- src/recompiler/mips/rec_lsu.cpp.h
+++ src/recompiler/mips/rec_lsu.cpp.h
@@ -20,9 +20,9 @@ static void disasm_psx(u32 pc)
 s32 imm_max, imm_min;
 
 /* NOTE: psxM must be mmap'ed, not malloc'ed, otherwise segfault */
-#ifdef USE_CONST_ADDRESSES
 static int LoadFromConstAddr(u32 insn)
 {
+#ifdef USE_CONST_ADDRESSES
 	if (IsConst(_Rs_)) {
 		u32 addr = iRegs[_Rs_].r + ((s32)(s16)_Imm_);
 		if ((addr & 0x1fffffff) < 0x200000) {
@@ -50,12 +50,14 @@ static int LoadFromConstAddr(u32 insn)
 			return 1;
 		}
 	}
+#endif
 
 	return 0;
 }
 
 static int StoreToConstAddr(u32 insn)
 {
+#ifdef USE_CONST_ADDRESSES
 	if (IsConst(_Rs_)) {
 		u32 addr = iRegs[_Rs_].r + ((s32)(s16)_Imm_);
 		/* DEBUGF("known address 0x%x", addr); */
@@ -82,15 +84,11 @@ static int StoreToConstAddr(u32 insn)
 			return 1;
 		}
 	}
+#endif
 
 	return 0;
 }
-#else
-static int LoadFromConstAddr(u32 insn) { (void)insn; return 0; }
-static int StoreToConstAddr(u32 insn) { (void)insn; return 0; }
-#endif
 
-#ifdef USE_DIRECT_MEM_ACCESS
 static void LoadFromAddr(u32 insn)
 {
 	// Rt = [Rs + imm16]
@@ -103,6 +101,8 @@ static void LoadFromAddr(u32 insn)
 	u32 r2 = regMipsToHost(rt, REG_FIND, REG_REGISTER);
 
 	ADDIU(MIPSREG_A0, r1, imm16);
+
+#ifdef USE_DIRECT_MEM_ACCESS
 	EXT(TEMP_1, MIPSREG_A0, 0, 0x1d); // and 0x1fffffff
 	LUI(TEMP_2, 0x80);
 	SLTU(TEMP_3, TEMP_1, TEMP_2);
@@ -126,6 +126,7 @@ static void LoadFromAddr(u32 insn)
 
 	// label_hle:
 	fixup_branch(backpatch1);
+#endif
 
 	switch (insn) {
 	case 0x80000000: CALLFunc((u32)psxMemRead8); SEB(r2, MIPSREG_V0); break; // LB
@@ -135,8 +136,10 @@ static void LoadFromAddr(u32 insn)
 	case 0x8c000000: CALLFunc((u32)psxMemRead32); MOV(r2, MIPSREG_V0); break; // LW
 	}
 
+#ifdef USE_DIRECT_MEM_ACCESS
 	// label_exit:
 	fixup_branch(backpatch2);
+#endif
 
 	regMipsChanged(rt);
 	regUnlock(r1);
@@ -155,6 +158,7 @@ static void StoreToAddr(u32 insn)
 
 	ADDIU(MIPSREG_A0, r1, imm16);
 
+#ifdef USE_DIRECT_MEM_ACCESS
 	/* First check if memory is writable atm */
 	LW(TEMP_1, PERM_REG_1, off(writeok));
 	backpatch3 = (u32 *)recMem;
@@ -192,6 +196,7 @@ static void StoreToAddr(u32 insn)
 	// label_hle:
 	fixup_branch(backpatch1);
 	fixup_branch(backpatch3);
+#endif
 
 	MOV(MIPSREG_A1, r2);
 	switch (insn) {
@@ -201,64 +206,14 @@ static void StoreToAddr(u32 insn)
 	default: break;
 	}
 
+#ifdef USE_DIRECT_MEM_ACCESS
 	// label_exit:
 	fixup_branch(backpatch2);
-
-	regUnlock(r1);
-	regUnlock(r2);
-}
-
-#else
-
-static void LoadFromAddr(u32 insn)
-{
-	// Rt = [Rs + imm16]
-	s32 imm16 = (s32)(s16)_Imm_;
-	u32 rs = _Rs_;
-	u32 rt = _Rt_;
-
-	u32 r1 = regMipsToHost(rs, REG_LOAD, REG_REGISTER);
-	u32 r2 = regMipsToHost(rt, REG_FIND, REG_REGISTER);
-
-	ADDIU(MIPSREG_A0, r1, imm16);
-
-	switch (insn) {
-	case 0x80000000: CALLFunc((u32)psxMemRead8); SEB(r2, MIPSREG_V0); break; // LB
-	case 0x90000000: CALLFunc((u32)psxMemRead8); MOV(r2, MIPSREG_V0); break; // LBU
-	case 0x84000000: CALLFunc((u32)psxMemRead16); SEH(r2, MIPSREG_V0); break; // LH
-	case 0x94000000: CALLFunc((u32)psxMemRead16); MOV(r2, MIPSREG_V0); break; // LHU
-	case 0x8c000000: CALLFunc((u32)psxMemRead32); MOV(r2, MIPSREG_V0); break; // LW
-	}
-
-	regMipsChanged(rt);
-	regUnlock(r1);
-	regUnlock(r2);
-}
-
-static void StoreToAddr(u32 insn)
-{
-	// mem[Rs + Im] = Rt
-	s32 imm16 = (s32)(s16)_Imm_;
-	u32 rs = _Rs_;
-	u32 rt = _Rt_;
-
-	u32 r1 = regMipsToHost(rs, REG_LOAD, REG_REGISTER);
-	u32 r2 = regMipsToHost(rt, REG_LOAD, REG_REGISTER);
-
-	ADDIU(MIPSREG_A0, r1, imm16);
-	MOV(MIPSREG_A1, r2);
-
-	switch (insn) {
-	case 0xa0000000: CALLFunc((u32)psxMemWrite8); break;
-	case 0xa4000000: CALLFunc((u32)psxMemWrite16); break;
-	case 0xac000000: CALLFunc((u32)psxMemWrite32); break;
-	default: break;
-	}
-
-	regUnlock(r1);
-	regUnlock(r2);
-}
 #endif
+
+	regUnlock(r1);
+	regUnlock(r2);
+}
 
 static int calc_loads()
 {
