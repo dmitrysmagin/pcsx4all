57f035e7ba482e9a425e1c903415136b05a8b9a0
Dmitry Smagin <exmortis@yandex.ru>
2016-05-06 10:20:31 +0300

    mipsrec: Simplify macros in rec_gte.cpp.h

--- src/recompiler/mips/rec_gte.cpp.h
+++ src/recompiler/mips/rec_gte.cpp.h
@@ -1,75 +1,58 @@
 #ifdef gte_old
 
-#define CP2_REGCACHE
-
-#define CP2_CALLFunc_Flush(func) \
-	CALLFunc(func)
-
-#define CP2_CALLFunc_NoFlush(func) \
-	CALLFunc(func)
-
-#define CP2_FUNC(f,n,cycle) \
+#define CP2_FUNC(f, cycle) \
 void gte##f(u32 code, u32 pc); void rec##f() \
 { \
-	if (autobias) cycles_pending+=cycle; \
-	CP2_REGCACHE \
-	{ \
-		/* TODO: Remove */regClearJump();	/**/ \
-		LI32(MIPSREG_A0, psxRegs.code); \
-		LI32(MIPSREG_A1, pc); \
-		CP2_CALLFunc_##n((u32)gte##f); \
-	} \
+	if (autobias) cycles_pending += cycle; \
+	/* TODO: Remove */ regClearJump(); \
+	LI32(MIPSREG_A0, psxRegs.code); \
+	LI32(MIPSREG_A1, pc); \
+	CALLFunc((u32)gte##f); \
 } \
 
-#define CP2_FUNC2(f,n,cycle) \
+#define CP2_FUNC2(f, cycle) \
 void gte##f(u32 code); void rec##f() \
 { \
-	if (autobias) cycles_pending+=cycle; \
-	CP2_REGCACHE \
-	{ \
-		/* TODO: Remove */regClearJump();	/**/ \
-		LI32(MIPSREG_A0, psxRegs.code); \
-		CP2_CALLFunc_##n((u32)gte##f); \
-	} \
+	if (autobias) cycles_pending += cycle; \
+	/* TODO: Remove */ regClearJump(); \
+	LI32(MIPSREG_A0, psxRegs.code); \
+	CALLFunc((u32)gte##f); \
 }
 
-#define CP2_FUNC3(f,n,cycle) \
+#define CP2_FUNC3(f, cycle) \
 void gte##f(); void rec##f() \
 { \
-	if (autobias) cycles_pending+=cycle; \
-	CP2_REGCACHE \
-	{ \
-		/* TODO: Remove */regClearJump();	/**/ \
-		CP2_CALLFunc_##n((u32)gte##f); \
-	} \
+	if (autobias) cycles_pending += cycle; \
+	/* TODO: Remove */ regClearJump(); \
+	CALLFunc((u32)gte##f); \
 }
 
-CP2_FUNC2(MFC2,Flush,2);
-CP2_FUNC2(MTC2,Flush,2);
-CP2_FUNC(LWC2,Flush,3);
-CP2_FUNC(SWC2,Flush,4);
-CP2_FUNC3(DCPL,NoFlush,8);
-CP2_FUNC3(RTPS,NoFlush,15);
-CP2_FUNC2(OP,NoFlush,6);
-CP2_FUNC3(NCLIP,NoFlush,8);
-CP2_FUNC3(DPCS,NoFlush,8);
-CP2_FUNC3(INTPL,NoFlush,8);
-CP2_FUNC2(MVMVA,NoFlush,8);
-CP2_FUNC3(NCDS,NoFlush,19);
-CP2_FUNC3(NCDT,NoFlush,44);
-CP2_FUNC3(CDP,NoFlush,13);
-CP2_FUNC3(NCCS,NoFlush,17);
-CP2_FUNC3(CC,NoFlush,11);
-CP2_FUNC3(NCS,NoFlush,14);
-CP2_FUNC3(NCT,NoFlush,30);
-CP2_FUNC2(SQR,NoFlush,3);
-CP2_FUNC3(DPCT,NoFlush,17);
-CP2_FUNC3(AVSZ3,NoFlush,5);
-CP2_FUNC3(AVSZ4,NoFlush,6);
-CP2_FUNC3(RTPT,NoFlush,23);
-CP2_FUNC2(GPF,NoFlush,5);
-CP2_FUNC2(GPL,NoFlush,5);
-CP2_FUNC3(NCCT,NoFlush,39);
+CP2_FUNC2(MFC2, 2);
+CP2_FUNC2(MTC2, 2);
+CP2_FUNC(LWC2, 3);
+CP2_FUNC(SWC2, 4);
+CP2_FUNC3(DCPL, 8);
+CP2_FUNC3(RTPS, 15);
+CP2_FUNC2(OP, 6);
+CP2_FUNC3(NCLIP, 8);
+CP2_FUNC3(DPCS, 8);
+CP2_FUNC3(INTPL, 8);
+CP2_FUNC2(MVMVA, 8);
+CP2_FUNC3(NCDS, 19);
+CP2_FUNC3(NCDT, 44);
+CP2_FUNC3(CDP, 13);
+CP2_FUNC3(NCCS, 17);
+CP2_FUNC3(CC, 11);
+CP2_FUNC3(NCS, 14);
+CP2_FUNC3(NCT, 30);
+CP2_FUNC2(SQR, 3);
+CP2_FUNC3(DPCT, 17);
+CP2_FUNC3(AVSZ3, 5);
+CP2_FUNC3(AVSZ4, 6);
+CP2_FUNC3(RTPT, 23);
+CP2_FUNC2(GPF, 5);
+CP2_FUNC2(GPL, 5);
+CP2_FUNC3(NCCT, 39);
 
 static void recCFC2()
 {
@@ -135,6 +118,7 @@ CP2_FUNC(NCCT, 39);
 
 static void recCFC2()
 {
+	if (autobias) cycles_pending += 2;
 	if (!_Rt_) return;
 
 	u32 rt = regMipsToHost(_Rt_, REG_FIND, REG_REGISTER);
@@ -146,6 +130,7 @@ static void recCFC2()
 
 static void recCTC2()
 {
+	if (autobias) cycles_pending += 2;
 	u32 rt = regMipsToHost(_Rt_, REG_LOAD, REG_REGISTER);
 	SW(rt, PERM_REG_1, offCP2C(_Rd_));
 	regUnlock(rt);
