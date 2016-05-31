26ad1c957ef3e3deecbcc341ad18e022aecb811b
Dmitry Smagin <exmortis@yandex.ru>
2016-05-31 16:58:39 +0300

    GZW-Zero: Add script for opk generation

---
+++ make_opk.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+OPK_NAME=pcsx4all.opk
+
+echo ${OPK_NAME}
+
+# create default.gcw0.desktop
+cat > default.gcw0.desktop <<EOF
+[Desktop Entry]
+Name=Pcsx4all
+Comment=Sony PSX emulator
+Exec=pcsx4all
+Terminal=false
+Type=Application
+StartupNotify=true
+Icon=pcsxr-icon-small
+Categories=emulators;
+EOF
+
+# create opk
+FLIST="pcsx4all"
+FLIST="${FLIST} default.gcw0.desktop"
+FLIST="${FLIST} src/port/sdl/pcsxr-icon-small.png"
+
+rm -f ${OPK_NAME}
+mksquashfs ${FLIST} ${OPK_NAME} -all-root -no-xattrs -noappend -no-exports
+
+cat default.gcw0.desktop
+rm -f default.gcw0.desktop
