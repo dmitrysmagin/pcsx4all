95f5fbc7101b1c03ea3b6f90f26ea28dd2d1f003
senquack <dansilsby@gmail.com>
2016-09-04 11:22:04 -0400

    gpu_unai: Cleanups/simplification of GPU command dispatch code

--- src/gpu/gpu_unai/gpu_command.h
+++ src/gpu/gpu_unai/gpu_command.h
@@ -74,159 +74,173 @@ void gpuSendPacketFunction(const int PRIM)
 
 	switch (PRIM)
 	{
-		case 0x02:
+		case 0x02: {
 			NULL_GPU();
 			gpuClearImage();    //  prim handles updateLace && skip
 			fb_dirty = true;
 			DO_LOG(("gpuClearImage(0x%x)\n",PRIM));
-			break;
+		} break;
+
 		case 0x20:
 		case 0x21:
 		case 0x22:
-		case 0x23:
+		case 0x23: {          // Monochrome 3-pt poly
 			if (!skipGPU)
 			{
 				NULL_GPU();
-				gpuDrawF3(gpuPolySpanDrivers [(blit_mask?512:0) | Blending_Mode | Masking | Blending | PixelMSB]);
+				PP driver = gpuPolySpanDrivers[(blit_mask?512:0) | Blending_Mode | Masking | Blending | PixelMSB];
+				gpuDrawF3(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawF3(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x24:
 		case 0x25:
 		case 0x26:
-		case 0x27:
+		case 0x27: {          // Textured 3-pt poly
 			if (!skipGPU)
 			{
 				NULL_GPU();
 				gpuSetCLUT    (PacketBuffer.U4[2] >> 16);
 				gpuSetTexture (PacketBuffer.U4[4] >> 16);
-				if ((PacketBuffer.U1[0]>0x5F) && (PacketBuffer.U1[1]>0x5F) && (PacketBuffer.U1[2]>0x5F))
-					gpuDrawFT3(gpuPolySpanDrivers [(blit_mask?512:0) | Blending_Mode | TEXT_MODE | Masking | Blending | PixelMSB]);
-				else
-					gpuDrawFT3(gpuPolySpanDrivers [(blit_mask?512:0) | Blending_Mode | TEXT_MODE | Masking | Blending | Lighting | PixelMSB]);
+				u32 driver_idx = (blit_mask?512:0) | Blending_Mode | TEXT_MODE | Masking | Blending | PixelMSB;
+				if (!((PacketBuffer.U1[0]>0x5F) && (PacketBuffer.U1[1]>0x5F) && (PacketBuffer.U1[2]>0x5F)))
+					driver_idx |= Lighting;
+				PP driver = gpuPolySpanDrivers[driver_idx];
+				gpuDrawFT3(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawFT3(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x28:
 		case 0x29:
 		case 0x2A:
-		case 0x2B:
+		case 0x2B: {          // Monochrome 4-pt poly
 			if (!skipGPU)
 			{
 				NULL_GPU();
-				const PP gpuPolySpanDriver  = gpuPolySpanDrivers [(blit_mask?512:0) | Blending_Mode | Masking | Blending | PixelMSB];
+				PP driver = gpuPolySpanDrivers[(blit_mask?512:0) | Blending_Mode | Masking | Blending | PixelMSB];
 				//--PacketBuffer.S2[6];
-				gpuDrawF3(gpuPolySpanDriver);
+				gpuDrawF3(driver);
 				PacketBuffer.U4[1] = PacketBuffer.U4[4];
 				//--PacketBuffer.S2[2];
-				gpuDrawF3(gpuPolySpanDriver);
+				gpuDrawF3(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawF4(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x2C:
 		case 0x2D:
 		case 0x2E:
-		case 0x2F:
+		case 0x2F: {          // Textured 4-pt poly
 			if (!skipGPU)
 			{
 				NULL_GPU();
 				gpuSetCLUT    (PacketBuffer.U4[2] >> 16);
 				gpuSetTexture (PacketBuffer.U4[4] >> 16);
-				PP gpuPolySpanDriver;
-				if ((PacketBuffer.U1[0]>0x5F) && (PacketBuffer.U1[1]>0x5F) && (PacketBuffer.U1[2]>0x5F))
-					gpuPolySpanDriver = gpuPolySpanDrivers [(blit_mask?512:0) | Blending_Mode | TEXT_MODE | Masking | Blending | PixelMSB];
-				else
-					gpuPolySpanDriver = gpuPolySpanDrivers [(blit_mask?512:0) | Blending_Mode | TEXT_MODE | Masking | Blending | Lighting | PixelMSB];
+				u32 driver_idx = (blit_mask?512:0) | Blending_Mode | TEXT_MODE | Masking | Blending | PixelMSB;
+				if (!((PacketBuffer.U1[0]>0x5F) && (PacketBuffer.U1[1]>0x5F) && (PacketBuffer.U1[2]>0x5F)))
+					driver_idx |= Lighting;
+				PP driver = gpuPolySpanDrivers[driver_idx];
 				//--PacketBuffer.S2[6];
-				gpuDrawFT3(gpuPolySpanDriver);
+				gpuDrawFT3(driver);
 				PacketBuffer.U4[1] = PacketBuffer.U4[7];
 				PacketBuffer.U4[2] = PacketBuffer.U4[8];
 				//--PacketBuffer.S2[2];
-				gpuDrawFT3(gpuPolySpanDriver);
+				gpuDrawFT3(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawFT4(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x30:
 		case 0x31:
 		case 0x32:
-		case 0x33:
+		case 0x33: {          // Gouraud-shaded 3-pt poly
 			if (!skipGPU)
 			{
 				NULL_GPU();
-				gpuDrawG3(gpuPolySpanDrivers [(blit_mask?512:0) | Blending_Mode | Masking | Blending | 129 | PixelMSB]);
+				PP driver = gpuPolySpanDrivers[(blit_mask?512:0) | Blending_Mode | Masking | Blending | 129 | PixelMSB];
+				gpuDrawG3(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawG3(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x34:
 		case 0x35:
 		case 0x36:
-		case 0x37:
+		case 0x37: {          // Gouraud-shaded, textured 3-pt poly
 			if (!skipGPU)
 			{
 				NULL_GPU();
 				gpuSetCLUT    (PacketBuffer.U4[2] >> 16);
 				gpuSetTexture (PacketBuffer.U4[5] >> 16);
-				gpuDrawGT3(gpuPolySpanDrivers [(blit_mask?512:0) | Blending_Mode | TEXT_MODE | Masking | Blending | ((Lighting)?129:0) | PixelMSB]);
+				PP driver = gpuPolySpanDrivers[(blit_mask?512:0) | Blending_Mode | TEXT_MODE | Masking | Blending | ((Lighting)?129:0) | PixelMSB];
+				gpuDrawGT3(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawGT3(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x38:
 		case 0x39:
 		case 0x3A:
-		case 0x3B:
+		case 0x3B: {          // Gouraud-shaded 4-pt poly
 			if (!skipGPU)
 			{
 				NULL_GPU();
-				const PP gpuPolySpanDriver  = gpuPolySpanDrivers [(blit_mask?512:0) | Blending_Mode | Masking | Blending | 129 | PixelMSB];
+				PP driver = gpuPolySpanDrivers[(blit_mask?512:0) | Blending_Mode | Masking | Blending | 129 | PixelMSB];
 				//--PacketBuffer.S2[6];
-				gpuDrawG3(gpuPolySpanDriver);
+				gpuDrawG3(driver);
 				PacketBuffer.U4[0] = PacketBuffer.U4[6];
 				PacketBuffer.U4[1] = PacketBuffer.U4[7];
 				//--PacketBuffer.S2[2];
-				gpuDrawG3(gpuPolySpanDriver);
+				gpuDrawG3(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawG4(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x3C:
 		case 0x3D:
 		case 0x3E:
-		case 0x3F:
+		case 0x3F: {          // Gouraud-shaded, textured 4-pt poly
 			if (!skipGPU)
 			{
 				NULL_GPU();
 				gpuSetCLUT    (PacketBuffer.U4[2] >> 16);
 				gpuSetTexture (PacketBuffer.U4[5] >> 16);
-				const PP gpuPolySpanDriver  = gpuPolySpanDrivers [(blit_mask?512:0) | Blending_Mode | TEXT_MODE | Masking | Blending | ((Lighting)?129:0) | PixelMSB];
+				PP driver = gpuPolySpanDrivers[(blit_mask?512:0) | Blending_Mode | TEXT_MODE | Masking | Blending | ((Lighting)?129:0) | PixelMSB];
 				//--PacketBuffer.S2[6];
-				gpuDrawGT3(gpuPolySpanDriver);
+				gpuDrawGT3(driver);
 				PacketBuffer.U4[0] = PacketBuffer.U4[9];
 				PacketBuffer.U4[1] = PacketBuffer.U4[10];
 				PacketBuffer.U4[2] = PacketBuffer.U4[11];
 				//--PacketBuffer.S2[2];
-				gpuDrawGT3(gpuPolySpanDriver);
+				gpuDrawGT3(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawGT4(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x40:
 		case 0x41:
 		case 0x42:
-		case 0x43:
+		case 0x43: {          // Monochrome line
 			if (!skipGPU)
 			{
 				NULL_GPU();
-				gpuDrawLF(gpuPixelDrivers [ (Blending_Mode | Masking | Blending | (PixelMSB>>3)) >> 1]);
+				PD driver = gpuPixelDrivers[(Blending_Mode | Masking | Blending | (PixelMSB>>3)) >> 1];
+				gpuDrawLF(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawLF(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x48:
 		case 0x49:
 		case 0x4A:
@@ -234,11 +248,12 @@ void gpuSendPacketFunction(const int PRIM)
 		case 0x4C:
 		case 0x4D:
 		case 0x4E:
-		case 0x4F:
+		case 0x4F: { // Monochrome line strip
 			if (!skipGPU)
 			{
 				NULL_GPU();
-				gpuDrawLF(gpuPixelDrivers [ (Blending_Mode | Masking | Blending | (PixelMSB>>3)) >> 1]);
+				PD driver = gpuPixelDrivers[(Blending_Mode | Masking | Blending | (PixelMSB>>3)) >> 1];
+				gpuDrawLF(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawLF(0x%x)\n",PRIM));
 			}
@@ -249,19 +264,22 @@ void gpuSendPacketFunction(const int PRIM)
 				PacketCount = 1;
 				PacketIndex = 3;
 			}
-			break;
+		} break;
+
 		case 0x50:
 		case 0x51:
 		case 0x52:
-		case 0x53:
+		case 0x53: {          // Gouraud-shaded line
 			if (!skipGPU)
 			{
 				NULL_GPU();
-				gpuDrawLG(gpuPixelDrivers [ (Blending_Mode | Masking | Blending | (PixelMSB>>3)) >> 1]);
+				PD driver = gpuPixelDrivers[(Blending_Mode | Masking | Blending | (PixelMSB>>3)) >> 1];
+				gpuDrawLG(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawLG(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x58:
 		case 0x59:
 		case 0x5A:
@@ -269,11 +287,12 @@ void gpuSendPacketFunction(const int PRIM)
 		case 0x5C:
 		case 0x5D:
 		case 0x5E:
-		case 0x5F:
+		case 0x5F: { // Gouraud-shaded line strip
 			if (!skipGPU)
 			{
 				NULL_GPU();
-				gpuDrawLG(gpuPixelDrivers [ (Blending_Mode | Masking | Blending | (PixelMSB>>3)) >> 1]);
+				PD driver = gpuPixelDrivers[(Blending_Mode | Masking | Blending | (PixelMSB>>3)) >> 1];
+				gpuDrawLG(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawLG(0x%x)\n",PRIM));
 			}
@@ -286,28 +305,33 @@ void gpuSendPacketFunction(const int PRIM)
 				PacketCount = 2;
 				PacketIndex = 3;
 			}
-			break;
+		} break;
+
 		case 0x60:
 		case 0x61:
 		case 0x62:
-		case 0x63:
+		case 0x63: {          // Monochrome rectangle (variable size)
 			if (!skipGPU)
 			{
 				NULL_GPU();
-				gpuDrawT(gpuTileSpanDrivers [Blending_Mode | Masking | Blending | (PixelMSB>>3)]);
+				PT driver = gpuTileSpanDrivers[Blending_Mode | Masking | Blending | (PixelMSB>>3)];
+				gpuDrawT(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawT(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x64:
 		case 0x65:
 		case 0x66:
-		case 0x67:
+		case 0x67: {          // Textured rectangle (variable size)
 			if (!skipGPU)
 			{
 				NULL_GPU();
 				gpuSetCLUT    (PacketBuffer.U4[2] >> 16);
 				gpuSetTexture (GPU_GP1);
+				u32 driver_idx = Blending_Mode | TEXT_MODE | Masking | Blending | (PixelMSB>>1);
+
 				// This fixes Silent Hill running animation on loading screens:
 				// (On PSX, color values 0x00-0x7F darken the source texture's color,
 				//  0x81-FF lighten textures (ultimately clamped to 0x1F),
@@ -321,74 +345,84 @@ void gpuSendPacketFunction(const int PRIM)
 				//  alone, I don't want to slow rendering down too much. (TODO)
 				//if ((PacketBuffer.U1[0]>0x5F) && (PacketBuffer.U1[1]>0x5F) && (PacketBuffer.U1[2]>0x5F))
 				// Strip lower 3 bits of each color and determine if lighting should be used:
-				if ((PacketBuffer.U4[0] & 0xF8F8F8) == 0x808080)
-					gpuDrawS(gpuSpriteSpanDrivers [Blending_Mode | TEXT_MODE | Masking | Blending | (PixelMSB>>1)]);
-				else
-					gpuDrawS(gpuSpriteSpanDrivers [Blending_Mode | TEXT_MODE | Masking | Blending | Lighting | (PixelMSB>>1)]);
+				if ((PacketBuffer.U4[0] & 0xF8F8F8) != 0x808080)
+					driver_idx |= Lighting;
+				PS driver = gpuSpriteSpanDrivers[driver_idx];
+				gpuDrawS(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawS(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x68:
 		case 0x69:
 		case 0x6A:
-		case 0x6B:
+		case 0x6B: {          // Monochrome rectangle (1x1 dot)
 			if (!skipGPU)
 			{
 				NULL_GPU();
 				PacketBuffer.U4[2] = 0x00010001;
-				gpuDrawT(gpuTileSpanDrivers [Blending_Mode | Masking | Blending | (PixelMSB>>3)]);
+				PT driver = gpuTileSpanDrivers[Blending_Mode | Masking | Blending | (PixelMSB>>3)];
+				gpuDrawT(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawT(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x70:
 		case 0x71:
 		case 0x72:
-		case 0x73:
+		case 0x73: {          // Monochrome rectangle (8x8)
 			if (!skipGPU)
 			{
 				NULL_GPU();
 				PacketBuffer.U4[2] = 0x00080008;
-				gpuDrawT(gpuTileSpanDrivers [Blending_Mode | Masking | Blending | (PixelMSB>>3)]);
+				PT driver = gpuTileSpanDrivers[Blending_Mode | Masking | Blending | (PixelMSB>>3)];
+				gpuDrawT(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawT(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x74:
 		case 0x75:
 		case 0x76:
-		case 0x77:
+		case 0x77: {          // Textured rectangle (8x8)
 			if (!skipGPU)
 			{
 				NULL_GPU();
 				PacketBuffer.U4[3] = 0x00080008;
 				gpuSetCLUT    (PacketBuffer.U4[2] >> 16);
 				gpuSetTexture (GPU_GP1);
+				u32 driver_idx = Blending_Mode | TEXT_MODE | Masking | Blending | (PixelMSB>>1);
+
 				//senquack - Only color 808080h-878787h allows skipping lighting calculation:
 				//if ((PacketBuffer.U1[0]>0x5F) && (PacketBuffer.U1[1]>0x5F) && (PacketBuffer.U1[2]>0x5F))
 				// Strip lower 3 bits of each color and determine if lighting should be used:
-				if ((PacketBuffer.U4[0] & 0xF8F8F8) == 0x808080)
-					gpuDrawS(gpuSpriteSpanDrivers [Blending_Mode | TEXT_MODE | Masking | Blending | (PixelMSB>>1)]);
-				else
-					gpuDrawS(gpuSpriteSpanDrivers [Blending_Mode | TEXT_MODE | Masking | Blending | Lighting | (PixelMSB>>1)]);
+				if ((PacketBuffer.U4[0] & 0xF8F8F8) != 0x808080)
+					driver_idx |= Lighting;
+				PS driver = gpuSpriteSpanDrivers[driver_idx];
+				gpuDrawS(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawS(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x78:
 		case 0x79:
 		case 0x7A:
-		case 0x7B:
+		case 0x7B: {          // Monochrome rectangle (16x16)
 			if (!skipGPU)
 			{
 				NULL_GPU();
 				PacketBuffer.U4[2] = 0x00100010;
-				gpuDrawT(gpuTileSpanDrivers [Blending_Mode | Masking | Blending | (PixelMSB>>3)]);
+				PT driver = gpuTileSpanDrivers[Blending_Mode | Masking | Blending | (PixelMSB>>3)];
+				gpuDrawT(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawT(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x7C:
 		case 0x7D:
 			#ifdef __arm__
@@ -403,24 +437,27 @@ void gpuSendPacketFunction(const int PRIM)
 			}
 			#endif
 		case 0x7E:
-		case 0x7F:
+		case 0x7F: {          // Textured rectangle (16x16)
 			if (!skipGPU)
 			{
 				NULL_GPU();
 				PacketBuffer.U4[3] = 0x00100010;
 				gpuSetCLUT    (PacketBuffer.U4[2] >> 16);
 				gpuSetTexture (GPU_GP1);
+				u32 driver_idx = Blending_Mode | TEXT_MODE | Masking | Blending | (PixelMSB>>1);
+
 				//senquack - Only color 808080h-878787h allows skipping lighting calculation:
 				//if ((PacketBuffer.U1[0]>0x5F) && (PacketBuffer.U1[1]>0x5F) && (PacketBuffer.U1[2]>0x5F))
 				// Strip lower 3 bits of each color and determine if lighting should be used:
-				if ((PacketBuffer.U4[0] & 0xF8F8F8) == 0x808080)
-					gpuDrawS(gpuSpriteSpanDrivers [Blending_Mode | TEXT_MODE | Masking | Blending | (PixelMSB>>1)]);
-				else
-					gpuDrawS(gpuSpriteSpanDrivers [Blending_Mode | TEXT_MODE | Masking | Blending | Lighting | (PixelMSB>>1)]);
+				if ((PacketBuffer.U4[0] & 0xF8F8F8) != 0x808080)
+					driver_idx |= Lighting;
+				PS driver = gpuSpriteSpanDrivers[driver_idx];
+				gpuDrawS(driver);
 				fb_dirty = true;
 				DO_LOG(("gpuDrawS(0x%x)\n",PRIM));
 			}
-			break;
+		} break;
+
 		case 0x80:          //  vid -> vid
 			gpuMoveImage();   //  prim handles updateLace && skip
 			if ((!skipCount) && (DisplayArea[3] == 480)) // Tekken 3 hack
