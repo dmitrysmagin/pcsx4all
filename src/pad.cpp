44ef1f03ff3e4c79f640745abe421b3a485c5810
Dmitry Smagin <exmortis@yandex.ru>
2016-12-05 22:55:51 +0300

    pad.cpp: Remove debug and profiler

--- src/pad.cpp
+++ src/pad.cpp
@@ -21,8 +21,6 @@
 #include "psxcommon.h"
 #include "psxmem.h"
 #include "r3000a.h"
-#include "profiler.h"
-#include "debug.h"
 
 typedef struct tagGlobalData
 {
@@ -35,23 +33,14 @@ typedef struct tagGlobalData
 static GLOBALDATA g={0,0,0,0};
 
 unsigned char PAD1_startPoll(void) {
-#ifdef DEBUG_ANALYSIS
-	dbg_anacnt_PAD1_startPoll++;
-#endif
 	g.CurByte1 = 0; return 0xFF;
 }
 
 unsigned char PAD2_startPoll(void) {
-#ifdef DEBUG_ANALYSIS
-	dbg_anacnt_PAD2_startPoll++;
-#endif
 	g.CurByte2 = 0; return 0xFF;
 }
 
 unsigned char PAD1_poll(void) {
-#ifdef DEBUG_ANALYSIS
-	dbg_anacnt_PAD1_poll++;
-#endif
 	static uint8_t		buf[8] = {0xFF, 0x5A, 0xFF, 0xFF, 0x80, 0x80, 0x80, 0x80};
 
 	if (g.CurByte1 == 0) {
@@ -59,80 +48,6 @@ unsigned char PAD1_poll(void) {
 		g.CurByte1++;
 
 		n = pad_read(0);
-#ifdef AUTOEVENTS
-		static unsigned event=0;
-		static unsigned short _pad1_=0xffff;
-		static clock_t t_init;
-		if (!event) t_init=clock();
-		event++;
-#ifdef SHOW_BIOS_CALLS
-		printf("EVENT %i\n",event);
-#endif
-#ifdef DEBUG_START
-		if (event>=DEBUG_START)
-			dbg_enable();
-#endif
-#ifdef AUTOEVENTS_MAX
-		if (event>AUTOEVENTS_MAX) {
-#ifndef DEBUG_PCSX4ALL
-			printf("TIME %.2fsec\n",((double)(clock()-t_init))/1000000.0);
-#endif
-			pcsx4all_exit();
-		}
-#endif
-#ifdef PROFILER_PCSX4ALL_RESET
-		static int prof_started=0;
-		if (event==PROFILER_PCSX4ALL_RESET)
-		{
-//puts("RESET!!!");fflush(stdout);
-			prof_started=1;
-			pcsx4all_prof_reset();
-			pcsx4all_prof_start(PCSX4ALL_PROF_CPU);
-		}
-#endif
-#ifdef DEBUG_CPU_OPCODES
-		{
-			char msg[50];
-			sprintf((char *)&msg[0],"Event %i MEMORY SUM",event);
-			dbgsum((char *)&msg[0],(void *)&psxM[0],0x200000);
-			dbgregsCop((void *)&psxRegs);
-		}
-#endif
-//printf("EVENTO %i\n",event); fflush(stdout);
-//		switch ((event>>1)&31)
-		switch (event&31)
-		{
-// /*
-			case 0: _pad1_ &= ~(1 << 12); break;
-			case 3: _pad1_ |= (1 << 12); break;
-// */
-// /*
-			case 12: _pad1_ |= (1 << 14); break;
-			case 15: _pad1_ &= ~(1 << 14); break;
-// */
-// /*
-			case 18: _pad1_ |= (1 << 15); break;
-			case 21: _pad1_ &= ~(1 << 15); break;
-// */
-// /*
-			case 6: _pad1_ &= ~(1 << 13); break;
-			case 9: _pad1_ |= (1 << 13); break;
-// */
-			case 24:
-#ifdef PROFILER_PCSX4ALL_RESET
-				 if (!prof_started)
-#endif
-				 _pad1_ &= ~(1 << 3);
-				 break;
-			case 26:
-#ifdef PROFILER_PCSX4ALL_RESET
-				 if (!prof_started)
-#endif
-				 _pad1_ |= (1 << 3);
-				 break;
-		}
-		n=_pad1_;
-#endif
 
 		buf[2] = n & 0xFF;
 		buf[3] = n >> 8;
@@ -147,20 +62,13 @@ unsigned char PAD1_poll(void) {
 }
 
 unsigned char PAD2_poll(void) {
-#ifdef DEBUG_ANALYSIS
-	dbg_anacnt_PAD2_poll++;
-#endif
 	static uint8_t		buf[8] = {0xFF, 0x5A, 0xFF, 0xFF, 0x80, 0x80, 0x80, 0x80};
 
 	if (g.CurByte2 == 0) {
 		uint16_t			n;
 		g.CurByte2++;
 
-#ifdef AUTOEVENTS
-		n = 0xffff;
-#else
 		n = pad_read(1);
-#endif
 
 		buf[2] = n & 0xFF;
 		buf[3] = n >> 8;
