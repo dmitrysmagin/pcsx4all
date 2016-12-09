dmitrysmagin/pcsx4all

commit 7351cbd9e5d2f03a29e24d1d9fbb99c5e4945960

Remove repetitive CHUI: comments

src/r3000a.h
@@ -29,8 +29,8 @@
 typedef struct {
 	int  (*Init)(void);
 	void (*Reset)(void);
-	void (*Execute)(void);		/* executes up to a break */
-	void (*ExecuteBlock)(unsigned target_pc);	/* executes up to a jump */
+	void (*Execute)(void);
+	void (*ExecuteBlock)(unsigned target_pc);
 	void (*Clear)(u32 Addr, u32 Size);
 	void (*Shutdown)(void);
 } R3000Acpu;
@@ -69,13 +69,13 @@ typedef union {
 typedef union {
 	struct {
 		u32	Index,     Random,    EntryLo0,  EntryLo1,
-						Context,   PageMask,  Wired,     Reserved0,
-						BadVAddr,  Count,     EntryHi,   Compare,
-						Status,    Cause,     EPC,       PRid,
-						Config,    LLAddr,    WatchLO,   WatchHI,
-						XContext,  Reserved1, Reserved2, Reserved3,
-						Reserved4, Reserved5, ECC,       CacheErr,
-						TagLo,     TagHi,     ErrorEPC,  Reserved6;
+			Context,   PageMask,  Wired,     Reserved0,
+			BadVAddr,  Count,     EntryHi,   Compare,
+			Status,    Cause,     EPC,       PRid,
+			Config,    LLAddr,    WatchLO,   WatchHI,
+			XContext,  Reserved1, Reserved2, Reserved3,
+			Reserved4, Reserved5, ECC,       CacheErr,
+			TagLo,     TagHi,     ErrorEPC,  Reserved6;
 	} n;
 	u32 r[32];
 	PAIR p[32];
@@ -152,22 +152,20 @@ typedef struct {
 	psxCP0Regs CP0;		/* Coprocessor0 Registers */
 	psxCP2Data CP2D; 	/* Cop2 data registers */
 	psxCP2Ctrl CP2C; 	/* Cop2 control registers */
-    u32 pc;				/* Program counter */
-    u32 code;			/* The instruction */
+	u32 pc;			/* Program counter */
+	u32 code;		/* The instruction */
 	u32 cycle;
 	u32 interrupt;
 
 	intCycle_t intCycle[32];
 
-// CHUI: Añado los ciclos hasta el proximo evento.
 	u32 io_cycle_counter;
-// CHUI: Añado el puntero a la memoria PSX
+
 	s8 *psxM;
 	s8 *psxP;
 	s8 *psxR;
 	s8 *psxH;
-// CHUI: Añadido para posible uso en el recompilador (psxRecLUT)
-	void *reserved;
+
 	int writeok;
 } psxRegisters;
 
@@ -243,16 +241,7 @@ extern psxRegisters psxRegs;
 
 #define _SetLink(x)     psxRegs.GPR.r[x] = _PC_ + 4;       // Sets the return address in the link register
 
-// CHUI: Macro que permite que en el proximo salto entre en psxBranchTest
-#ifdef DEBUG_CPU_OPCODES
-#define ResetIoCycle() { \
-	if (psxRegs.io_cycle_counter) { \
-		psxRegs.io_cycle_counter=0; \
-	} \
-} 
-#else
-#define ResetIoCycle() psxRegs.io_cycle_counter=0;
-#endif
+#define ResetIoCycle() do { psxRegs.io_cycle_counter = 0; } while (0)
 
 extern int  psxInit(void);
 extern void psxReset(void);
@@ -263,6 +252,5 @@ extern void psxExecuteBios(void);
 extern int  psxTestLoadDelay(int reg, u32 tmp);
 extern void psxDelayTest(int reg, u32 bpc);
 extern void psxTestSWInts(void);
-
 
 #endif /* __R3000A_H__ */

src/psxbios.cpp
@@ -1413,7 +1413,6 @@ void psxBios_StartRCnt(void) { // 04
 #endif
 
 	a0&= 0x3;
-// CHUI: Añado ResetIoCycle para permite que en el proximo salto entre en psxBranchTest
 	ResetIoCycle();
 	if (a0 != 3) psxHu32ref(0x1074)|= SWAP32((u32)((1<<(a0+4))));
 	else psxHu32ref(0x1074)|= SWAPu32(0x1);
@@ -1426,7 +1425,6 @@ void psxBios_StopRCnt(void) { // 05
 #endif
 
 	a0&= 0x3;
-// CHUI: Añado ResetIoCycle para permite que en el proximo salto entre en psxBranchTest
 	ResetIoCycle();
 	if (a0 != 3) psxHu32ref(0x1074)&= SWAP32((u32)(~(1<<(a0+4))));
 	else psxHu32ref(0x1074)&= SWAPu32(~0x1);
@@ -1694,7 +1692,6 @@ void psxBios_StartPAD(void) { // 13
 #endif
 
 	pad_stopped = 0;
-// CHUI: Añado ResetIoCycle para permite que en el proximo salto entre en psxBranchTest
 	ResetIoCycle();
 	psxHwWrite16(0x1f801074, (unsigned short)(psxHwRead16(0x1f801074) | 0x1));
 	psxRegs.CP0.n.Status |= 0x401;
@@ -1721,7 +1718,6 @@ void psxBios_PAD_init(void) { // 15
 	PSXBIOS_LOG("psxBios_%s\n", biosB0n[0x15]);
 #endif
 
-// CHUI: Añado ResetIoCycle para permite que en el proximo salto entre en psxBranchTest
 	ResetIoCycle();
 	psxHwWrite16(0x1f801074, (u16)(psxHwRead16(0x1f801074) | 0x1));
 	pad_buf = (int*)Ra1;
@@ -1755,7 +1751,6 @@ void psxBios_ReturnFromException(void) { // 17
 
 	psxRegs.CP0.n.Status = (psxRegs.CP0.n.Status & 0xfffffff0) |
 						  ((psxRegs.CP0.n.Status & 0x3c) >> 2);
-// CHUI: Añado ResetIoCycle para permite que en el proximo salto entre en psxBranchTest
 	ResetIoCycle();
 }
 
@@ -2917,7 +2912,6 @@ void biosInterrupt(void) {
 					softCall(RcEV[i][1].fhandler);
 				}
 				psxHwWrite32(0x1f801070, ~(1 << (i+4)));
-// CHUI: Añado ResetIoCycle para permite que en el proximo salto entre en psxBranchTest
 				ResetIoCycle();
 			}
 		}
@@ -2949,7 +2943,6 @@ void psxBiosException(void) {
 			if (jmp_int != NULL) {
 
 				psxHwWrite32(0x1f801070, 0xffffffff);
-// CHUI: Añado ResetIoCycle para permite que en el proximo salto entre en psxBranchTest
 				ResetIoCycle();
 
 				ra = jmp_int[0];
@@ -2966,7 +2959,6 @@ void psxBiosException(void) {
 				return;
 			}
 			psxHwWrite16(0x1f801070, 0);
-// CHUI: Añado ResetIoCycle para permite que en el proximo salto entre en psxBranchTest
 			ResetIoCycle();
 			break;
 
@@ -2974,7 +2966,6 @@ void psxBiosException(void) {
 			switch (a0) {
 				case 1: // EnterCritical - disable irq's
 					psxRegs.CP0.n.Status&=~0x404; 
-// CHUI: Añado ResetIoCycle para permite que en el proximo salto entre en psxBranchTest
 					//v0=1;	// HDHOSHY experimental patch: Spongebob, Coldblood, fearEffect, Medievil2, Martian Gothic
 					if (autobias)
 						psxRegs.cycle+=624;
@@ -2982,7 +2973,6 @@ void psxBiosException(void) {
 
 				case 2: // ExitCritical - enable irq's
 					psxRegs.CP0.n.Status|= 0x404; 
-// CHUI: Añado ResetIoCycle para permite que en el proximo salto entre en psxBranchTest
 					if (autobias)
 						psxRegs.cycle+=464;
 					break;
@@ -2993,7 +2983,6 @@ void psxBiosException(void) {
 
 			psxRegs.CP0.n.Status = (psxRegs.CP0.n.Status & 0xfffffff0) |
 								  ((psxRegs.CP0.n.Status & 0x3c) >> 2);
-// CHUI: Añado ResetIoCycle para permite que en el proximo salto entre en psxBranchTest
 			ResetIoCycle();
 			return;
 
@@ -3006,7 +2995,6 @@ void psxBiosException(void) {
 
 	psxRegs.CP0.n.Status = (psxRegs.CP0.n.Status & 0xfffffff0) |
 						  ((psxRegs.CP0.n.Status & 0x3c) >> 2);
-// CHUI: Añado ResetIoCycle para permite que en el proximo salto entre en psxBranchTest
 	ResetIoCycle();
 }
 

src/psxcounters.cpp
@@ -123,7 +123,6 @@ static u32 &psxNextsCounter = psxRegs.intCycle[PSXINT_RCNT].sCycle;
 static inline void setIrq( u32 irq )
 {
 	psxHu32ref(0x1070) |= SWAPu32(irq);
-// CHUI: Añado ResetIoCycle para permite que en el proximo salto entre en psxBranchTest
     	ResetIoCycle();
 }
 

commit f0e32f61b85c9b9344b3dd6e34e8ef2f75887fb5

r3000a: Add 'reserved' var back to psxRegs struct
Removing it in commit 7351cbd9e5d2f03a29e24d1d9fbb99c5e4945960
broke compatibility with older savestates.

src/r3000a.h
@@ -166,6 +166,7 @@ typedef struct {
 	s8 *psxR;
 	s8 *psxH;
 
+	void *reserved;
 	int writeok;
 } psxRegisters;
 