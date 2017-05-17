46840ee862d5811dba4871bca536266fb2c5e9b9
senquack <dansilsby@gmail.com>
2017-05-17 08:05:19 -0400

    mipsrec: Optimize DIV/DIVU emitter even more
    Eliminated all branching in div-by-0 fixup code.
    
    Eliminated div-by-0 fixup code entirely if PS1 GCC auto-
    generated exception code sequence is found. (See prior
    commit that added ability to scan for this.)  The sequence
    would have crashed the PS1 if division by zero ever occurred.
    
    Also, from testing on a JZ4770 GCW Zero, I have found that the HI
    register contains the same result as PS1 did when div-by-0 occurs,
    so there is no need to fix it up on that platform.

--- src/recompiler/mips/rec_mdu.cpp.h
+++ src/recompiler/mips/rec_mdu.cpp.h
@@ -1,3 +1,13 @@
+// If your platform gives the same results as PS1 did in the HI register
+//   when dividing by zero, define this to eliminate some fixup code, which
+//   MIPS32 jz-series CPUs seem to. (See notes in recDIV()/recDIVU())
+#if defined(GCW_ZERO)
+	#define OMIT_DIV_BY_ZERO_HI_FIXUP
+#endif
+
+// See notes in recDIV()/recDIVU (disable for debugging purposes)
+#define OMIT_DIV_BY_ZERO_FIXUP_IF_EXCEPTION_SEQUENCE_FOUND
+
 static void recMULT() {
 // Lo/Hi = Rs * Rt (signed)
 	if (!(_Rs_) || !(_Rt_)) {
@@ -52,36 +62,52 @@ static void recDIV()
 	//       modern MIPS32 CPUs like the jz4770 of GCW Zero. Unfortunately,
 	//       division-by-zero results only match PS1 in the 'hi' reg.
 	//  Rs              Rt       Hi/Remainder  Lo/Result
-	//  0..+7FFFFFFFh   0   -->  Rs            -1
-	//  -80000000h..-1  0   -->  Rs            +1
-	//  -80000000h      -1  -->  0             -80000000h
+	//  0..+7FFFFFFFh    0   -->  Rs           -1
+	//  -80000000h..-1   0   -->  Rs           +1
+	//  -80000000h      -1   -->  0           -80000000h
+
+	bool omit_div_by_zero_fixup = false;
 
 	if (IsConst(_Rt_) && iRegs[_Rt_].r != 0) {
+		// If divisor is known-const val and isn't 0, no need to fixup
+		omit_div_by_zero_fixup = true;
+	} else if (!branch) {
+#ifdef OMIT_DIV_BY_ZERO_FIXUP_IF_EXCEPTION_SEQUENCE_FOUND
+		// If we're not inside a branch-delay slot, we scan ahead for a
+		//  PS1 GCC auto-generated code sequence that was inserted in most
+		//  binaries after a DIV/DIVU instruction. The sequence checked for
+		//  div-by-zero (and signed overflow, for DIV) and issued an
+		//  exception that went unhandled by PS1 BIOS, which would crash.
+		//  If found, don't bother to emulate PS1 div-by-zero result.
+		//
+		// Sequence is sometimes preceeded by one or two MFHI/MFLO opcodes.
+		u32 code_loc = pc;
+		code_loc += 4 * rec_scan_for_MFHI_MFLO_sequence(code_loc);
+		omit_div_by_zero_fixup = (rec_scan_for_div_by_zero_check_sequence(code_loc) > 0);
+#endif
+	}
+
+	if (omit_div_by_zero_fixup) {
 		DIV(rs, rt);
-		MFLO(TEMP_1); /* NOTE: Hi/Lo can't be cached for now, so spill them */
+		MFLO(TEMP_1);              // NOTE: Hi/Lo can't be cached for now, so spill them
 		MFHI(TEMP_2);
 	} else {
-		u32 *backpatch1, *backpatch2;
-		backpatch1 = (u32*)recMem;
-		BEQZ(rt, 0);
-		ADDIU(TEMP_2, 0, -1);  // $t1 = -1  <BD slot>
-
 		DIV(rs, rt);
-		MFLO(TEMP_1); /* NOTE: Hi/Lo can't be cached for now, so spill them */
+		ADDIU(MIPSREG_A1, 0, -1);
+		SLT(TEMP_3, rs, 0);        // TEMP_3 = (rs < 0 ? 1 : 0)
+		MFLO(TEMP_1);              // NOTE: Hi/Lo can't be cached for now, so spill them
+		MFHI(TEMP_2);
 
-		backpatch2 = (u32*)recMem;
-		B(0);
-		MFHI(TEMP_2);  // <BD slot>
+		// If divisor was 0, set LO result (quotient) to 1 if dividend was < 0
+		// If divisor was 0, set LO result (quotient) to -1 if dividend was >= 0
+		MOVN(MIPSREG_A0, TEMP_3, TEMP_3);      // if (TEMP_3 != 0) then MIPSREG_A1 = TEMP_3
+		MOVZ(MIPSREG_A0, MIPSREG_A1, TEMP_3);  // if (TEMP_3 == 0) then MIPSREG_A1 = MIPSREG_A0
+		MOVZ(TEMP_1, MIPSREG_A0, rt);          // if (rt == 0) then TEMP_1 = MIPSREG_A0
 
-		// division by zero
-		fixup_branch(backpatch1);
-
-		SLT(TEMP_1, rs, 0); // $t0 = (rs < 0 ? 1 : 0)
-		MOVZ(TEMP_1, TEMP_2, TEMP_1); // if $t0 == 0 then $t0 = $t1
-		MOV(TEMP_2, rs);
-
-		// exit
-		fixup_branch(backpatch2);
+#ifndef OMIT_DIV_BY_ZERO_HI_FIXUP
+		// If divisor was 0, set HI result (remainder) to rs
+		MOVZ(TEMP_2, rs, rt);
+#endif
 	}
 
 	SW(TEMP_1, PERM_REG_1, offGPR(32));
@@ -97,39 +123,40 @@ static void recDIVU()
 	u32 rt = regMipsToHost(_Rt_, REG_LOAD, REG_REGISTER);
 
 	// Test if divisor is 0, emulating correct results for PS1 CPU.
-	// NOTE: we don't bother checking for signed division overflow (the
-	//       last entry in this list), as it seems to work the same on
-	//       modern MIPS32 CPUs like the jz4770 of GCW Zero. Unfortunately,
-	//       division-by-zero results only match PS1 in the 'hi' reg.
 	//  Rs              Rt       Hi/Remainder  Lo/Result
-	//  0..+7FFFFFFFh   0   -->  Rs            -1
-	//  -80000000h..-1  0   -->  Rs            +1
-	//  -80000000h      -1  -->  0             -80000000h
+	//  0..FFFFFFFFh    0   -->  Rs            FFFFFFFFh
+
+	bool omit_div_by_zero_fixup = false;
 
 	if (IsConst(_Rt_) && iRegs[_Rt_].r != 0) {
+		// If divisor is known-const val and isn't 0, no need to fixup
+		omit_div_by_zero_fixup = true;
+	} else if (!branch) {
+#ifdef OMIT_DIV_BY_ZERO_FIXUP_IF_EXCEPTION_SEQUENCE_FOUND
+		// See notes in recDIV() emitter
+		u32 code_loc = pc;
+		code_loc += 4 * rec_scan_for_MFHI_MFLO_sequence(code_loc);
+		omit_div_by_zero_fixup = (rec_scan_for_div_by_zero_check_sequence(code_loc) > 0);
+#endif
+	}
+
+	if (omit_div_by_zero_fixup) {
 		DIVU(rs, rt);
-		MFLO(TEMP_1); /* NOTE: Hi/Lo can't be cached for now, so spill them */
+		MFLO(TEMP_1);              // NOTE: Hi/Lo can't be cached for now, so spill them
 		MFHI(TEMP_2);
 	} else {
-		u32 *backpatch1, *backpatch2;
-		backpatch1 = (u32*)recMem;
-		BEQZ(rt, 0);
-		ADDIU(TEMP_1, 0, -1);  // $t0 = -1  <BD slot>
-
 		DIVU(rs, rt);
-		MFLO(TEMP_1); /* NOTE: Hi/Lo can't be cached for now, so spill them */
+		ADDIU(TEMP_3, 0, -1);
+		MFLO(TEMP_1);              // NOTE: Hi/Lo can't be cached for now, so spill them
+		MFHI(TEMP_2);
 
-		backpatch2 = (u32*)recMem;
-		B(0);
-		MFHI(TEMP_2);  // <BD slot>
+		// If divisor was 0, set LO result (quotient) to 0xffff_ffff
+		MOVZ(TEMP_1, TEMP_3, rt);  // if (rt == 0) then TEMP_1 = TEMP_3
 
-		// division by zero
-		fixup_branch(backpatch1);
-
-		MOV(TEMP_2, rs);
-
-		// exit
-		fixup_branch(backpatch2);
+#ifndef OMIT_DIV_BY_ZERO_HI_FIXUP
+		// If divisor was 0, set HI result (remainder) to rs
+		MOVZ(TEMP_2, rs, rt);
+#endif
 	}
 
 	SW(TEMP_1, PERM_REG_1, offGPR(32));
