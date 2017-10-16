9f4399b401aabeaefad5e75d2aaf4d4b84be33d1
senquack <dansilsby@gmail.com>
2017-10-15 23:55:05 -0400

    mipsrec: Emit MIPS32r2 op for common 2-op ALU sequences
    When original opcode params allow it:
    SLL,SRA 24/16 -> SEB/SEH
    SLL,SRL       -> INS zeroes
    SRL,SLL       -> INS zeroes
    SRA,SLL       -> INS zeroes
    SRL,ANDI      -> EXT

--- src/recompiler/mips/rec_alu.cpp.h
+++ src/recompiler/mips/rec_alu.cpp.h
@@ -1,4 +1,14 @@
-/* NOTE: There's no need to do zero register optimizatins since we have
+/***********************************************
+ * Options that can be disabled for debugging: *
+ ***********************************************/
+
+/* Convert various MIPS I opcode sequences to a modern opcode */
+#if defined(HAVE_MIPS32R2_EXT_INS) && defined(HAVE_MIPS32R2_SEB_SEH)
+#define USE_MIPS32R2_ALU_OPCODE_CONVERSION
+#endif
+
+
+/* NOTE: There's no need to do zero register optimizations since we have
          native zero reg on mips. */
 #define REC_ITYPE_RT_RS_I16(insn, _rt_, _rs_, _imm_) \
 do { \
@@ -170,8 +180,107 @@ do { \
 	regUnlock(r2); \
 } while (0)
 
+
 static void recSLL()
 {
+#ifdef USE_MIPS32R2_ALU_OPCODE_CONVERSION
+	if (!branch)
+	{
+		const u32 next_opcode = OPCODE_AT(pc);
+
+		// If next opcode is SRA, see if we can convert this SLL,SRA sequence
+		//  into a newer MIPS32r2 'SEB' or 'SEH' instruction instead.
+		// Sequence we're looking for is:
+		//   SLL   rd = rt << sa    (this opcode)
+		//   SRA   rd = rt >> sa    (next opcode)
+		// Where the rd of SLL is the same as the rd,rt of ANDI,
+		//  and both shift amounts are equal and are either 24 or 16.
+
+		if (_fOp_(next_opcode) == 0 && _fFunct_(next_opcode) == 0x03 &&  // SRA
+		    (_Sa_ == 24 || _Sa_ == 16) && _Sa_ == _fSa_(next_opcode) &&
+		    _Rd_ == _fRd_(next_opcode) && _Rd_ == _fRt_(next_opcode))
+		{
+			u32 r1, r2;
+			if (_Rd_ == _Rt_) {
+				r1 = regMipsToHost(_Rd_, REG_LOAD, REG_REGISTER);
+				r2 = r1;
+			} else {
+				r1 = regMipsToHost(_Rd_, REG_FIND, REG_REGISTER);
+				r2 = regMipsToHost(_Rt_, REG_LOAD, REG_REGISTER);
+			}
+
+			if (_Sa_ == 16) {
+				SEH(r1, r2);
+			} else {
+				SEB(r1, r2);
+			}
+
+			regMipsChanged(_Rd_);
+			regUnlock(r1);
+			regUnlock(r2);
+
+			// Propagate constness of result.
+			if (iRegs[_Rt_].s)
+				SetConst(_Rd_, ((s32)(iRegs[_Rt_].r << _Sa_) >> _Sa_));
+			else
+				SetUndef(_Rd_);
+
+			// Skip the second opcode, disassembling it if disasm is enabled.
+			DISASM_PSX(pc);
+			DISASM_MSG("CONVERTING SLL,SRA SEQUENCE TO %s\n", _Sa_==16 ? "SEH" : "SEB");
+			pc += 4;
+
+			// Success.. we're done
+			return;
+		}
+
+		// If next opcode is SRL, see if we can convert this SLL,SRL sequence
+		//  into a newer MIPS32r2 'INS' instruction instead (inserting 0s).
+		// Sequence we're looking for is:
+		//   SLL   rd = rt << sa    (this opcode)
+		//   SRL   rd = rt >> sa    (next opcode)
+		// Where all the rd,rt of both opcodes are all the same and both
+		//  shift amounts are equal.
+		if ((_fOp_(next_opcode) == 0 && _fFunct_(next_opcode) == 0x2) &&  // SRL
+		    _Sa_ != 0                                                 &&
+		    _Sa_ == _fSa_(next_opcode)                                &&
+		    _Rd_ == _Rt_                                              &&
+		    _Rd_ == _fRd_(next_opcode)                                &&
+		    _Rd_ == _fRt_(next_opcode))
+		{
+			u32 r1, r2;
+			if (_Rd_ == _Rt_) {
+				r1 = regMipsToHost(_Rd_, REG_LOAD, REG_REGISTER);
+				r2 = r1;
+			} else {
+				r1 = regMipsToHost(_Rd_, REG_FIND, REG_REGISTER);
+				r2 = regMipsToHost(_Rt_, REG_LOAD, REG_REGISTER);
+			}
+
+			INS(r1, 0, (32 - _Sa_), _Sa_);
+
+			regMipsChanged(_Rd_);
+			regUnlock(r1);
+			regUnlock(r2);
+
+			// Propagate constness of result.
+			if (iRegs[_Rt_].s)
+				SetConst(_Rd_, (((u32)iRegs[_Rt_].r << _Sa_) >> _Sa_));
+			else
+				SetUndef(_Rd_);
+
+			// Skip the second opcode, disassembling it if disasm is enabled.
+			DISASM_PSX(pc);
+			DISASM_MSG("CONVERTING SLL,SRL SEQUENCE TO MIPS32R2 INS\n");
+			pc += 4;
+
+			// Success.. we're done
+			return;
+		}
+	}
+#endif // USE_MIPS32R2_ALU_OPCODE_CONVERSION
+
+
 	u32 s = iRegs[_Rt_].s;
 
 	REC_RTYPE_RD_RT_SA(SLL, _Rd_, _Rt_, _Sa_);
@@ -180,8 +289,116 @@ static void recSLL()
 		SetConst(_Rd_, iRegs[_Rt_].r << _Sa_);
 }
 
+
 static void recSRL()
 {
+#ifdef USE_MIPS32R2_ALU_OPCODE_CONVERSION
+	if (!branch)
+	{
+		const u32 next_opcode = OPCODE_AT(pc);
+
+		// If next opcode is ANDI, see if we can convert this SRL,ANDI sequence
+		//  into a newer MIPS32r2 'EXT' instruction instead.
+		// Sequence we're looking for is:
+		//   SRL   rd = rt >> sa    (this opcode)
+		//   ANDI  rt = rs & imm    (next opcode)
+		// Where the rd of SRL is the same as the rs,rt of ANDI
+
+		if (_fOp_(next_opcode) == 0x0c &&  // ANDI
+		    _fImmU_(next_opcode) != 0  &&
+		    _Rd_ == _fRs_(next_opcode) && _Rd_ == _fRt_(next_opcode))
+		{
+			// Determine if ANDI's imm is one less than a power of two,
+			//  i.e. it is 0x1,0x3,0x7,0xf, ... 0x3fff,0x7fff, 0xffff,
+			//  i.e., with no trailing or intervening 0s.
+			//  Note that we've already checked that imm is not 0.
+			const u32 imm = _fImmU_(next_opcode);
+			if (((imm+1) & imm) == 0)
+			{
+				// Number of bits for 'ext' to extract: count trailing zeroes
+				//  of imm+1, starting at LSB.
+				const u32 num_bits = __builtin_ctz(imm+1);
+				// Position to start extracting from is the SRL's shift-amount
+				const u32 pos_to_extract = _Sa_;
+
+				u32 r1, r2;
+				if (_Rd_ == _Rt_) {
+					r1 = regMipsToHost(_Rd_, REG_LOAD, REG_REGISTER);
+					r2 = r1;
+				} else {
+					r1 = regMipsToHost(_Rd_, REG_FIND, REG_REGISTER);
+					r2 = regMipsToHost(_Rt_, REG_LOAD, REG_REGISTER);
+				}
+
+				EXT(r1, r2, pos_to_extract, num_bits);
+
+				regMipsChanged(_Rd_);
+				regUnlock(r1);
+				regUnlock(r2);
+
+				// Propagate constness of result.
+				if (iRegs[_Rt_].s)
+					SetConst(_Rd_, (((u32)iRegs[_Rt_].r >> _Sa_) & _fImmU_(next_opcode)));
+				else
+					SetUndef(_Rd_);
+
+				// Skip the second opcode, disassembling it if disasm is enabled.
+				DISASM_PSX(pc);
+				DISASM_MSG("CONVERTING SRL,ANDI SEQUENCE TO MIPS32R2 EXT\n");
+				pc += 4;
+
+				// Success.. we're done
+				return;
+			}
+		}
+
+		// If next opcode is SLL, see if we can convert this SRL,SLL sequence
+		//  into a newer MIPS32r2 'INS' instruction instead (inserting 0s).
+		// Sequence we're looking for is:
+		//   SRL   rd = rt >> sa    (this opcode)
+		//   SLL   rd = rt << sa    (next opcode)
+		// Where all the rd,rt of both opcodes are all the same and both
+		//  shift amounts are equal.
+		if ((_fOp_(next_opcode) == 0 && _fFunct_(next_opcode) == 0) &&  // SLL
+		    _Sa_ != 0                                               &&
+		    _Sa_ == _fSa_(next_opcode)                              &&
+		    _Rd_ == _Rt_                                            &&
+		    _Rd_ == _fRd_(next_opcode)                              &&
+		    _fRd_(next_opcode) == _fRt_(next_opcode))
+		{
+			u32 r1, r2;
+			if (_Rd_ == _Rt_) {
+				r1 = regMipsToHost(_Rd_, REG_LOAD, REG_REGISTER);
+				r2 = r1;
+			} else {
+				r1 = regMipsToHost(_Rd_, REG_FIND, REG_REGISTER);
+				r2 = regMipsToHost(_Rt_, REG_LOAD, REG_REGISTER);
+			}
+
+			INS(r1, 0, 0, _Sa_);
+
+			regMipsChanged(_Rd_);
+			regUnlock(r1);
+			regUnlock(r2);
+
+			// Propagate constness of result.
+			if (iRegs[_Rt_].s)
+				SetConst(_Rd_, (((u32)iRegs[_Rt_].r >> _Sa_) << _Sa_));
+			else
+				SetUndef(_Rd_);
+
+			// Skip the second opcode, disassembling it if disasm is enabled.
+			DISASM_PSX(pc);
+			DISASM_MSG("CONVERTING SRL,SLL SEQUENCE TO MIPS32R2 INS\n");
+			pc += 4;
+
+			// Success.. we're done
+			return;
+		}
+	}
+#endif // USE_MIPS32R2_ALU_OPCODE_CONVERSION
+
+
 	u32 s = iRegs[_Rt_].s;
 
 	REC_RTYPE_RD_RT_SA(SRL, _Rd_, _Rt_, _Sa_);
@@ -190,8 +407,61 @@ static void recSRL()
 		SetConst(_Rd_, (u32)iRegs[_Rt_].r >> _Sa_);
 }
 
+
 static void recSRA()
 {
+#ifdef USE_MIPS32R2_ALU_OPCODE_CONVERSION
+	if (!branch)
+	{
+		const u32 next_opcode = OPCODE_AT(pc);
+
+		// If next opcode is SLL, see if we can convert this SRA,SLL sequence
+		//  into a newer MIPS32r2 'INS' instruction instead (inserting 0s).
+		// Sequence we're looking for is:
+		//   SRA   rd = rt >> sa    (this opcode)
+		//   SLL   rd = rt << sa    (next opcode)
+		// Where all the rd,rt of both opcodes are all the same and both
+		//  shift amounts are equal.
+		if ((_fOp_(next_opcode) == 0 && _fFunct_(next_opcode) == 0) &&  // SLL
+		    _Sa_ != 0                                               &&
+		    _Sa_ == _fSa_(next_opcode)                              &&
+		    _Rd_ == _Rt_                                            &&
+		    _Rd_ == _fRd_(next_opcode)                              &&
+		    _fRd_(next_opcode) == _fRt_(next_opcode))
+		{
+			u32 r1, r2;
+			if (_Rd_ == _Rt_) {
+				r1 = regMipsToHost(_Rd_, REG_LOAD, REG_REGISTER);
+				r2 = r1;
+			} else {
+				r1 = regMipsToHost(_Rd_, REG_FIND, REG_REGISTER);
+				r2 = regMipsToHost(_Rt_, REG_LOAD, REG_REGISTER);
+			}
+
+			INS(r1, 0, 0, _Sa_);
+
+			regMipsChanged(_Rd_);
+			regUnlock(r1);
+			regUnlock(r2);
+
+			// Propagate constness of result.
+			if (iRegs[_Rt_].s)
+				SetConst(_Rd_, (((s32)iRegs[_Rt_].r >> _Sa_) << _Sa_));
+			else
+				SetUndef(_Rd_);
+
+			// Skip the second opcode, disassembling it if disasm is enabled.
+			DISASM_PSX(pc);
+			DISASM_MSG("CONVERTING SRA,SLL SEQUENCE TO MIPS32R2 INS\n");
+			pc += 4;
+
+			// Success.. we're done
+			return;
+		}
+	}
+#endif // USE_MIPS32R2_ALU_OPCODE_CONVERSION
+
+
 	u32 s = iRegs[_Rt_].s;
 
 	REC_RTYPE_RD_RT_SA(SRA, _Rd_, _Rt_, _Sa_);
@@ -199,6 +469,7 @@ static void recSRA()
 	if (s)
 		SetConst(_Rd_, (s32)iRegs[_Rt_].r >> _Sa_);
 }
+
 
 #define REC_RTYPE_RD_RT_RS(insn, _rd_, _rt_, _rs_) \
 do { \
